8cea997884eccd172c05cfb3b74361a6e4afb5d7
Stephen Stewart <stephen.stewart@carisenda.com>
2016-08-16 15:45:26 +0100

    add react-router and some simple routes

--- src/containers/app.js
+++ src/containers/app.js
@@ -1,12 +1,20 @@
-import React, { Component } from 'react';
+import React, { PropTypes, Component } from 'react';
+import { Link } from 'react-router';
 
 export default class App extends Component {
   render() {
     return (
-      <div>1Hello again, world!</div>
+      <div>
+        <div>
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+        </div>
+        {this.props.children}
+      </div>
     );
   }
 }
 
 App.propTypes = {
+  children: PropTypes.node.isRequired
 };
